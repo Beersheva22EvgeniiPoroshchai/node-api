2ff480495364da891b3e0fb3e7d9c21558604d7e
Joebye <evgenii.prs@gmail.com>
2023-10-10 00:22:29 +0300

    node multithreading workpool-solution

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import MongoConnection from "./MongoConnection.mjs";
 import express from 'express';
+import workerpool from 'workerpool'
+const pool = workerpool.pool('./totalThread.mjs')   //link to the file containing thread registration in pool
+
 const app = express();
 const port = process.env.PORT || 8181;
 const server = app.listen(port);
@@ -12,11 +15,23 @@ const studentsCollection = dbConnection.getCollection('students')
 app.get('/performance/total', (req,res) => {
     const startTime = new Date();
     const count = +req.query.count;
-    let total = 0;
-    for (let i = 0; i < count; i++) {
-        total++;
-    }
-    res.send({pid: process.pid, api: 'node', total, time: new Date().getTime() - startTime.getTime()});
+    pool.exec('total', [count], {
+        on: payload => {
+            //res.send(payload.data);
+            if (payload.event == 'partition') {
+                res.write(payload.data + '\n');
+                console.log(payload.data);
+            } else {
+                res.end(JSON.stringify(payload.data))
+            }
+
+        }
+    })
+    // let total = 0;
+    // for (let i = 0; i < count; i++) {
+    //     total++;
+    // };
+    
 });
 app.get('/performance/students', async (req, res) => {
     const startTime = new Date();
---
+++ totalThread.mjs
@@ -0,0 +1,26 @@
+import workerpool from 'workerpool';
+function totalThread(count) {
+    const startTime = new Date();
+    let total = 0;
+    const partition = Math.trunc(count/5)
+    for (let i=1; i<=count; i++) {
+        if(i % partition == 0) {
+            const nPartition = i / partition;
+           // console.log(`${nPartition}. partition`);
+           workerpool.workerEmit({event: 'partition', data: `${nPartition}.partition`})
+        }
+        total++;
+    }
+    workerpool.workerEmit({event: 'end',
+     data:
+     {api: 'node',
+      total,
+       time: new Date().getTime() - startTime.getTime()
+    }
+});
+
+}
+//registration the thread functionality as any key and a functional obj as a value
+workerpool.worker({
+    total: totalThread
+})
